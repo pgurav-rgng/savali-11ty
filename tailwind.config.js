bdcb6b0408e95624d8e6f27e766b4b5a32fa06da
pgurav <pradeep.gurav@gmail.com>
2025-06-08 00:45:08 +0530

    Fix missing dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js,njk,md}", // Adjust this path to include all your Eleventy templates
-    "./public/**/*.{html,js,njk,md}", // If Eleventy processes files in public
-  ],
+    "./src/**/*.{html,js,njk,md}",
+    "./public/**/*.html",
+    "./_includes/**/*.{html,njk}",   // add this if using Eleventy's includes folder
+    "./_layouts/**/*.{html,njk}",    // or similar if your layouts are elsewhere
+  ],  
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        yellow: tailwind.colors.yellow,
+        gray: tailwind.colors.gray,
+        primary: {
+          50: '#f0fdf4',
+          100: '#dcfce7',
+          200: '#bbf7d0',
+          300: '#86efac',
+          400: '#4ade80',
+          500: '#22c55e',
+          600: '#16a34a',
+          700: '#15803d',
+          800: '#166534',
+          900: '#14532d',
+        },
+      },
+    },
   },
-  plugins: [], // This array should be empty if you're not using official Tailwind plugins
+  safelist: [
+    "bg-yellow-500",
+    "text-gray-900",
+    "fixed",
+    "top-4",
+    "left-1/2",
+    "transform",
+    "-translate-x-1/2",
+    "px-4",
+    "py-2",
+    "rounded-lg",
+    "shadow-lg",
+    "text-sm",
+  ],
+  plugins: [],
 };
